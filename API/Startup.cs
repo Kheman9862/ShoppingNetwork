64ac9dab7934e7b8b9fce5a230c0dc0d696984eb
kheman <garg.kheman1996@gmail.com>
2021-06-13 15:49:05 -0400

    Adding Redis DB for basket

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,6 +7,7 @@ using Infrastructure.Data;
 using API.Helpers;
 using API.Middleware;
 using API.Extensions;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -31,6 +32,13 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x=>x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             
+            services.AddSingleton<IConnectionMultiplexer>(c=>
+            {               
+                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuration);
+                }
+            );
+
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddAutoMapper(typeof(MappingProfiles));
